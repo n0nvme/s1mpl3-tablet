ad7f47908d6e0a52ef2d54f2899c5d035328949b
n0nvme <svmpl3nvm3@gmail.com>
2019-09-02 16:51:57 +0300

    resize wibar

--- rc.lua
+++ rc.lua
@@ -257,7 +257,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist_buttons)
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 35 })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
